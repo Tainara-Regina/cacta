2b2518cdd2464f01a6d79659126a6e60e1ef4f6c
Tainara <tainararegina20@gmail.com>
2020-01-12 02:53:58 -0300

    ajuste slide mobile

--- public/js/slick.js
+++ public/js/slick.js
@@ -18,7 +18,7 @@ jQuery(document).ready(function($) {
         {
            breakpoint: 400,
            settings: {
-              arrows: false,
+              arrows: true,
               slidesToShow: 1,
               slidesToScroll: 1
            }
