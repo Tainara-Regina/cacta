4cfaca33eb79f2f1eb2a75905d57a115e6fea105
Tainara <tainararegina20@gmail.com>
2020-01-12 02:31:39 -0300

    ajuste menu

--- resources/views/site/includes/menu.blade.php
+++ resources/views/site/includes/menu.blade.php
@@ -4,28 +4,8 @@
   <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target=".navbar-collapse">
     ☰
   </button>
-  <div class="collapse navbar-collapse" id="navbarNavDropdown"  style="width:9%;">
-    <ul class="navbar-nav">
-       <li class="nav-item active ml-2">
-        <a class="nav-link" href="#"><b style="color: green">Logar</b><span class="sr-only">(current)</span></a>
-      </li>
-      <li class="nav-item active">
-        <a class="nav-link" href="#">Cadastre-se</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Contrate</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Cacta blog</a>
-      </li>
-    </ul>
-  </div>
-  <a class="navbar-brand mx-auto text-center" href="#"> <img class="animated rubberBand cacto" style="height: 50px;" src="https://image.flaticon.com/icons/png/512/43/43369.png"> 
-   <p class="text-center mb-0 pb-0 logo">Cacta</p>
- </a>
- <div class="navbar-collapse collapse">
-  <ul class="nav navbar-nav ml-auto logo-redes">
-
+  <div class="collapse navbar-collapse" id="navbarNavDropdown"  style="width:27%;">
+ <ul class="nav navbar-nav logo-redes">
     <li class="nav-item">
       <a class="nav-link" href="#"> <a class="nav-link" href="#">
         <i class="fa fa-instagram"></i>
@@ -41,5 +21,59 @@
       </a>
     </li>
   </ul>
+
+
+    <!-- <ul class="navbar-nav">
+       <li class="nav-item active ml-2">
+        <a class="nav-link" href="#"><b style="color: green">Logar</b><span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Cadastre-se</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Contrate</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Cacta blog</a>
+      </li>
+    </ul> -->
+  </div>
+  <a class="navbar-brand mx-auto text-center" href="#"> <img class="animated rubberBand cacto" style="height: 50px;" src="https://image.flaticon.com/icons/png/512/43/43369.png"> 
+   <p class="text-center mb-0 pb-0 logo">Cacta</p>
+ </a>
+ <div class="navbar-collapse collapse">
+ <ul class="navbar-nav">
+       <li class="nav-item active ml-2">
+        <a class="nav-link" href="#"><b style="color: green">Logar</b><span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Cadastre-se</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Contrate</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Cacta blog</a>
+      </li>
+    </ul>
+
+
+
+ <!--  <ul class="nav navbar-nav ml-auto logo-redes">
+    <li class="nav-item">
+      <a class="nav-link" href="#"> <a class="nav-link" href="#">
+        <i class="fa fa-instagram"></i>
+      </a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="#"> <a class="nav-link" href="#"><i class="fa fa-facebook"></i>
+      </a>
+    </li>
+
+    <li class="nav-item">
+      <a class="nav-link" href="#"> <a class="nav-link" href="#"><i class="fa fa-pinterest"></i>
+      </a>
+    </li>
+  </ul> -->
 </div>
 </nav>
