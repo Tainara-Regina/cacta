9fd66216998da6e4ed2a74be3bd23bdc777ae648
Tainara <tainararegina20@gmail.com>
2020-01-12 11:47:35 -0300

    ajuste mobile

--- resources/views/site/includes/menu-mobile.blade.php
+++ resources/views/site/includes/menu-mobile.blade.php
@@ -1,5 +1,5 @@
 
-<nav id="NavMobile" class="navbar navbar-expand-lg navbar-light bg-light menu">
+<nav id="NavMobile" class="navbar navbar-expand-lg navbar-light bg-light menu p-0">
    <a class="navbar-brand mx-auto text-center" href="#"> <img class="animated rubberBand cacto" style="height: 35px;" src="https://image.flaticon.com/icons/png/512/43/43369.png"> 
    <p class="text-center mb-0 pb-0 logo">Cacta</p>
  </a>
