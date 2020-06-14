05961d2e4df908921400258524081881f9385478
Tainara <tainararegina20@gmail.com>
2020-06-14 16:54:31 -0300

    criar vaga

--- app/Http/Controllers/VagaController.php
+++ app/Http/Controllers/VagaController.php
@@ -95,8 +95,10 @@ public function listaVaga(){
 
 	$contratar     = ContratarHome::select()->first();
 	$fundo_vaga    = DB::table('fundo_vaga_home')
-	->inRandomOrder()
-	->first();
+		->inRandomOrder()
+		->where('disponivel', 1)
+		->first();
+		
 	$procurar_vaga = ProcurarVagaHome::select()->first();
 		//dd($contratar->all());
 	return view('site.lista-vagas',compact('contratar','fundo_vaga','procurar_vaga','vagas','segmentos'));
--- resources/views/site/includes/menu-mobile.blade.php
+++ resources/views/site/includes/menu-mobile.blade.php
@@ -1,7 +1,7 @@
 
 <nav id="NavMobile" class="navbar navbar-expand-lg navbar-light bg-light menu p-0">
-   <a class="navbar-brand mx-auto text-center" href="{{route('site.inicio')}}"> <img class="animated rubberBand cacto" style="height: 35px;" src="{{asset('/img/logo_cacta.png')}}"> 
-     <p class="text-center mb-0 pb-0 logo">Cacta Vagas</p>
+ <a class="navbar-brand mx-auto text-center" href="{{route('site.inicio')}}"> <img class="animated rubberBand cacto" style="height: 35px;" src="{{asset('/img/logo_cacta.png')}}"> 
+   <p class="text-center mb-0 pb-0 logo">Cacta Vagas</p>
  </a>
  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
   <span class="navbar-toggler-icon"></span>
@@ -11,26 +11,31 @@
     <li class="nav-item dropdown">
       <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
         <b style="color: green">Entrar</b>
-    </a>
-    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+      </a>
+      <div class="dropdown-menu" aria-labelledby="navbarDropdown">
         <a class="dropdown-item" href="#" data-toggle="modal" data-target="#modalCandidato">Área do candidato</a>
         <div class="dropdown-divider"></div>
         <a class="dropdown-item" href="#" data-toggle="modal" data-target="#modalContratante">Área do contratante</a>
-    </div>
-</li>
-@if(!Request::session()->get('menu_candidato'))
-<li class="nav-item active">
-  <a class="nav-link" href="{{route('formularioCandidato')}}">Cadastre-se</a>
-</li>
-@endif
-@if(!Request::session()->get('menu_contratante'))
-<li class="nav-item active">
-  <a class="nav-link"  href="{{route('formularioContratante')}}">Contrate</a>
-</li>
-@endif
-<li class="nav-item active">
-  <a class="nav-link" href="{{route('blog-home')}}">Cacta Vagas Blog</a>
-</li>
-</ul>
+      </div>
+    </li>
+    @if(!Request::session()->get('menu_candidato'))
+    <li class="nav-item active">
+      <a class="nav-link" href="{{route('formularioCandidato')}}">Cadastre-se</a>
+    </li>
+    @endif
+    @if(!Request::session()->get('menu_contratante'))
+    <li class="nav-item active">
+      <a class="nav-link"  href="{{route('formularioContratante')}}">Contrate</a>
+    </li>
+    @endif
+
+    <li class="nav-item active">
+      <a class="nav-link" href="{{route('site.lista-vaga')}}">Vagas</a>
+    </li>
+
+    <li class="nav-item active">
+      <a class="nav-link" href="{{route('blog-home')}}">Cacta Vagas Blog</a>
+    </li>
+  </ul>
 </div>
 </nav>
