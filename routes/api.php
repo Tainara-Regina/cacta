46d78a39a863f9e73a02b325d8134a204e25acdd
Tainara <tainararegina20@gmail.com>
2020-06-21 16:05:34 -0300

    ajuste

--- routes/api.php
+++ routes/api.php
@@ -25,8 +25,21 @@ Route::post('/pagarme', function (Request $request) {
 //Slack::to('#cacta-vagas')->send($request->id);
 
 
-$teste = $request->id;
-Slack::to('#cacta-vagas')->send($teste);
+$id             = $request->id; //ID da transação.
+$event          = $request->event; //A qual evento o postback se refere. No caso de transações: transaction_status_changed. Já para subscriptions: subscription_status_changed.
+$old_status     = $request->old_status; //Status anterior da transação.
+$desired_status = $request->desired_status;// Status ideal para objetos deste tipo, em um fluxo normal, onde autorização e captura são feitos com sucesso, por exemplo.
+$current_status = $request->current_status;// 	Status para o qual efetivamente mudou.
+$object         = $request->object; //Qual o tipo do objeto referido. No caso de transações o valor é 'transaction'. No caso de assinaturas, o valor é 'subscription'
+$transaction    = $request->transaction; //Possui todas as informações do objeto. Para acessar objetos internos basta acessar a chave transaction[objeto1][objeto2]. Ex: para acessar o ddd: transaction[phone][ddd] Postback - transação
+
+
+
+
+
+
+
+Slack::to('#cacta-vagas')->send(" ID da transação: ".$id.". Status para o qual efetivamente mudou:  ".$current_status.". Status anterior da transação: ".$old_status.". Qual o tipo do objeto referido: ".$object);
 dd($request->id);
 // $requestBody = file_get_contents("php://input"); 
 // $signature = $_SERVER['HTTP_X_HUB_SIGNATURE'];
