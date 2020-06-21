bc795a97babc85ad49f9d9b274cb86a96f610220
Tainara <tainararegina20@gmail.com>
2020-06-21 15:09:57 -0300

    ajuste

--- routes/api.php
+++ routes/api.php
@@ -22,8 +22,8 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 
 Route::post('/pagarme', function (Request $request) {
  Slack::to('#cacta-vagas')->send('Um postback foi enviado!');
-Slack::to('#cacta-vagas')->send($request->id);
-//dd($request->all());
+//Slack::to('#cacta-vagas')->send($request->id);
+dd($request->id);
 // $requestBody = file_get_contents("php://input"); 
 // $signature = $_SERVER['HTTP_X_HUB_SIGNATURE'];
 
