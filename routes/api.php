6410773e92daa71afc0a4d789abfaef2f32139b1
Tainara <tainararegina20@gmail.com>
2020-06-21 13:12:38 -0300

    retur post

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,48 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+
+
+Route::post('/pagarme', function () {
+	return Slack::to('#cacta-vagas')->send('Um postback foi enviado!');
+	//return view('site.pagarme');
+  // echo 'banana';
+  
+
+// $pagarme = new \PagarMe\Client('ak_test_aEZCKKiNyBscZ2DZ3qjy69LB6A46qs');
+
+// $subscription = $pagarme->subscriptions()->getList();
+
+// dd($subscription);
+
+
+
+
+
+
+
+// $transaction = $pagarme->transactions()->create([
+//   'amount' => 5200,
+//   'payment_method' => 'boleto',
+//   'async' => false,
+//   'customer' => [
+//     'external_id' => '1',
+//     'name' => 'Nome do cliente',
+//     'type' => 'individual',
+//     'country' => 'br',
+//     'documents' => [
+//       [
+//         'type' => 'cpf',
+//         'number' => '00000000000'
+//       ]
+//     ],
+//     'phone_numbers' => [ '+551199999999' ],
+//     'email' => 'cliente@email.com'
+//   ]
+// ]);
+
+
+
+});
